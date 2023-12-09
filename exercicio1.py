ff8f55c8059aed39af18f6a384830801e52201c8
LsBarbosa2004 <lucas.barbosa@sou.inteli.edu.br>
2023-12-09 18:55:16 -0300

    Adiciona exercícios

--- exercicio1.py
+++ exercicio1.py
@@ -1,18 +1,14 @@
-#  Se achar necessario, faça import de outras bibliotecas
-
-
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
+def conta_palavras_unicas(frase):
+    palavras = frase.lower().split()
+    quantidade_palavras_unicas = len(set(palavras))
+    return quantidade_palavras_unicas
 
 # Teste a sua função aqui (caso ache necessário)
+frase = "banana maçã maçã maçã maçã maçã maçã maçã maçã maçã maçã maçã maçã maçã"
+resultado = conta_palavras_unicas(frase)
+
+print("Palavras únicas:", resultado)
+
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -1,18 +1,16 @@
-#  Se achar necessario, faça import de outras bibliotecas
-
-
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
+def is_anagram(str1, str2):
+    return sorted(str1) == sorted(str2)
 
 # Teste a sua função aqui (caso ache necessário)
+print(is_anagram("banana", "banana")) 
+print(is_anagram("banana", "maçã")) 
+print(is_anagram("amor", "roma")) 
+print(is_anagram("pedra", "padre")) 
+print(is_anagram("pedra", "pedro")) 
+print(is_anagram("perda", "pedra")) 
+print(is_anagram("perda", "pedro")) 
+print(is_anagram("perda", "padre")) 
+
 
 
 
